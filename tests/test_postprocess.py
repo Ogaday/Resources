83f0529df5db40c38329ca4cd0fc32b5526462a9
Ogaday <W.Ogaday@gmail.com>
2015-10-09 16:04:19 +0100

    Create tests directory. Create skeleton for postprocess test file

---
+++ tests/test_postprocess.py
@@ -0,0 +1,8 @@
+# Collect list of samples already run on the computer
+# ie. cases = [entry.name for entry in os.scandir() if finished(entry.name)] etc.
+# Then for each case, extract the already calculated power in the central spreadsheet, compare to within enough decimal places accuracy. There is some discrepency at 10e-14.
+# Done! 
+
+# Also test all of the utility methods as well.
+
+pass
