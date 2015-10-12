ce778e2eaa1ca2cf0cbf5ca7d95fe303f9e8bcd5
Ogaday <w.ogaday@gmail.com>
2015-10-12 18:31:51 +0100

    Delete testing line

--- run-simulations-remote.sh
+++ run-simulations-remote.sh
@@ -14,10 +14,6 @@ jobs=( "09.04R" "09.10R" )
 echo "length hosts: ${#hosts[@]}"
 echo "length jobs: ${#jobs[@]}"
 
-if [ "5" -lt "10" ]; then
-	echo "True!"
-fi
-
 if [ "${#jobs[@]}" -lt "${#hosts[@]}" ]; then
     echo "Length of jobs is shorter: ${#jobs[@]}"
     extent=${#jobs[@]}
@@ -28,6 +24,6 @@ fi
 
 for ((i=0;i<$extent;++i)); do
   echo "${hosts[i]} runs ${jobs[i]}"
-  scp /scratch/wow203/CFD_Files/${jobs[i]} wow203@${hosts[i]}:"/scratch/wow203/OpenFOAM/wow203-2.1.0/run/generation${jobs[i]:0:2}/${jobs[i]}"
+  scp -r /scratch/wow203/CFD_Files/${jobs[i]} wow203@${hosts[i]}:"/scratch/wow203/OpenFOAM/wow203-2.1.0/run/generation${jobs[i]:0:2}/${jobs[i]}"
   ssh wow203@${hosts[i]} cd "/scratch/wow203/OpenFOAM/wow203-2.1.0/run/generation${jobs[i]:0:2}/${jobs[i]}" && blockMesh && topoSet && setsToZones && setFields && TInterfoam &> log &  
 done
