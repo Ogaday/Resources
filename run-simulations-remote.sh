7f128d90112e13e5c4aa6cdc7d3a46a8f1a017a6
Ogaday <w.ogaday@gmail.com>
2015-10-13 13:31:38 +0100

    Update run simulations! Finally works! Added a command to run a script LOCALLY which attaches and detaches a screen around the simulation process

--- run-simulations-remote.sh
+++ run-simulations-remote.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-# Execute this script in a screen so it can be detached
+# Execute this script on emps-smetana
 
 for host in $(cat hosts.txt); do
   echo $host
 done
 
-#readarray -t hosts < hosts.txt
-#readarray -t jobs < jobs.txt
+readarray -t hosts < hosts.txt
+readarray -t jobs < jobs.txt
 
-hosts=( "blue03" "blue04" )
-jobs=( "11.02" "11.03" )
+#hosts=( "blue03" "blue04" )
+#jobs=( "11.02" "11.03" )
 
 echo "length hosts: ${#hosts[@]}"
 echo "length jobs: ${#jobs[@]}"
@@ -24,11 +24,12 @@ fi
 
 for ((i=0;i<$extent;++i)); do
   echo "${hosts[i]} runs ${jobs[i]}"
-  if [ ! -a "/scratch/wow203/OpenFOAM/wow203-2.1.0/run/generation${jobs[i]:0:2}/${jobs[i]}/run.sh" ]
-    then
-      scp /scratch/wow203/CFD_Files/${jobs[i]} ~/Project/Resources/run.sh wow203@${hosts[i]}:/scratch/wow203/OpenFOAM/wow203-2.1.0/run/generation${jobs[i]:0:2}/${jobs[i]}/
-  else
+  cp ~/Project/Resources/run.sh /scratch/wow203/CFD_Files/${jobs[i]}
+  #if [ ! -a "/scratch/wow203/OpenFOAM/wow203-2.1.0/run/generation${jobs[i]:0:2}/${jobs[i]}/run.sh" ]
+  #  then
+  #    scp -r /scratch/wow203/CFD_Files/${jobs[i]} ~/Project/Resources/run.sh wow203@${hosts[i]}:/scratch/wow203/OpenFOAM/wow203-2.1.0/run/generation${jobs[i]:0:2}/${jobs[i]}/
+  #else
     scp -r /scratch/wow203/CFD_Files/${jobs[i]} wow203@${hosts[i]}:"/scratch/wow203/OpenFOAM/wow203-2.1.0/run/generation${jobs[i]:0:2}/${jobs[i]}"
-  fi
+  #fi
   ssh wow203@${hosts[i]} "cd /scratch/wow203/OpenFOAM/wow203-2.1.0/run/generation${jobs[i]:0:2}/${jobs[i]} && blockMesh && topoSet && setsToZones && setFields && ./run.sh"
 done
