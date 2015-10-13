f946fa74cca408bc1fd68347317066309f0c9610
Ogaday <W.Ogaday@gmail.com>
2015-10-13 10:50:02 +0100

    Update run-simulations-remote. Most hopefull I've been so yet.

--- run-simulations-remote.sh
+++ run-simulations-remote.sh
@@ -8,8 +8,8 @@ done
 #readarray -t hosts < hosts.txt
 #readarray -t jobs < jobs.txt
 
-hosts=( "blue01" "blue02" )
-jobs=( "09.04R" "09.10R" )
+hosts=( "blue03" "blue04" )
+jobs=( "11.02" "11.03" )
 
 echo "length hosts: ${#hosts[@]}"
 echo "length jobs: ${#jobs[@]}"
@@ -24,6 +24,11 @@ fi
 
 for ((i=0;i<$extent;++i)); do
   echo "${hosts[i]} runs ${jobs[i]}"
-  scp -r /scratch/wow203/CFD_Files/${jobs[i]} wow203@${hosts[i]}:"/scratch/wow203/OpenFOAM/wow203-2.1.0/run/generation${jobs[i]:0:2}/${jobs[i]}"
-  ssh wow203@${hosts[i]} cd "/scratch/wow203/OpenFOAM/wow203-2.1.0/run/generation${jobs[i]:0:2}/${jobs[i]}" && blockMesh && topoSet && setsToZones && setFields && TInterfoam &> log &  
+  if [ ! -a "/scratch/wow203/OpenFOAM/wow203-2.1.0/run/generation${jobs[i]:0:2}/${jobs[i]}/run.sh" ]
+    then
+      scp /scratch/wow203/CFD_Files/${jobs[i]} ~/Project/Resources/run.sh wow203@${hosts[i]}:/scratch/wow203/OpenFOAM/wow203-2.1.0/run/generation${jobs[i]:0:2}/${jobs[i]}/
+  else
+    scp -r /scratch/wow203/CFD_Files/${jobs[i]} wow203@${hosts[i]}:"/scratch/wow203/OpenFOAM/wow203-2.1.0/run/generation${jobs[i]:0:2}/${jobs[i]}"
+  fi
+  ssh wow203@${hosts[i]} "cd /scratch/wow203/OpenFOAM/wow203-2.1.0/run/generation${jobs[i]:0:2}/${jobs[i]} && blockMesh && topoSet && setsToZones && setFields && ./run.sh"
 done
