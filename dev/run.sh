8a5e6dbae147355b7d95f2a730e4c606233afa11
Ogaday <w.ogaday@gmail.com>
2015-10-20 13:41:29 +0100

    Do some testing and clean the screen names

--- dev/run.sh
+++ dev/run.sh
@@ -16,8 +16,8 @@ solid=${soldir#*generation*/}
 echo "solution dir is $soldir"
 echo "solution id is $solid"
 
-#screen -d -m -S "screen${args[1]}.${args[2]}" -t "${args[1]}.${args[2]}"
-#screen -S "screen${args[1]}.${args[2]}" -p "${args[1]}.${args[2]}" -X stuff "echo \"solution dir is $soldir\"$(printf \\r)"
+screen -d -m -S "screen-$solid" -t "$solid"
+screen -S "screen-$solid" -p "$solid" -X stuff "echo \"solution dir is $soldir\"$(printf \\r)"
 
 # One method, for instance:
 # screen -d -m -S test_screen
