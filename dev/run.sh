896b7c4f24b1d777ed4359155c3a8232785d16ec
Ogaday <w.ogaday@gmail.com>
2015-10-20 13:32:44 +0100

    Solve running script in screen\!

--- dev/run.sh
+++ dev/run.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
-screen -R
-blockMesh && topoSet && setsToZones && setFields && TInterFoam &> log &
-screen -d
+args=("$@")
+argnum=${#args[@]}
+
+rundir=/scratch/wow203/OpenFOAM/wow203-2.1.0/run
+
+if [ $argnum -eq 0 ]; then
+  soldir=$PWD
+elif [ $argnum -eq 2 ]; then
+  soldir="$rundir/generation${args[0]}/${args[0]}.${args[1]}"
+elif [ $argnum -eq 3 ]; then
+  rundir=${args[0]}
+  soldir="$rundir/generation${args[1]}/${args[1]}.${args[2]}"
+fi
+solid=${soldir#*generation*/}
+echo "solution dir is $soldir"
+echo "solution id is $solid"
+
+#screen -d -m -S "screen${args[1]}.${args[2]}" -t "${args[1]}.${args[2]}"
+#screen -S "screen${args[1]}.${args[2]}" -p "${args[1]}.${args[2]}" -X stuff "echo \"solution dir is $soldir\"$(printf \\r)"
+
+# One method, for instance:
+# screen -d -m -S test_screen
+# screen -S test_screen -p 0 -X stuff "sleep 5 && touch ~/Project/Resources/dev/hello_world && sleep 1$(printf \\r)"
+
+
+#screen -R
+#blockMesh && topoSet && setsToZones && setFields && TInterFoam &> log &
+#screen -d
 
