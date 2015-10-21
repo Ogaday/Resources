d2f404c94f99bebbc08a185c6f1ee8d77ab77c23
Ogaday <w.ogaday@gmail.com>
2015-10-21 11:52:25 +0100

    Write first version of remote run

--- dev/remote-run.sh
+++ dev/remote-run.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+echo "args are $@"
+args=("$@")
+casepath=${args[0]}
+genid=${args[1]}
+solid=$genid.${args[2]}
+remotehost=${args[3]}
+
+echo "casepath is $casepath"
+echo "genid is $genid"
+echo "solid is $solid"
+echo remotehost "is $remotehost"
+
+ssh wow203@$remotehost "mkdir -p /scratch/wow203/OpenFOAM/wow203-2.1.0/run/generation$genid/"
+scp -r path wow203@remotehost:/scratch/wow203/OpenFOAM/wow203-2.1.0/run/generation$genid/$solid
+ssh wow203@$remotehost "cd /scratch/wow203/OpenFOAM/wow203-2.1.0/run/generation$genid/$solid && ~/Project/Resources/dev/run.sh $genid ${args[2]}"
