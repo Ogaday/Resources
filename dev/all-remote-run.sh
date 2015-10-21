d40b059a587275f786e575790cd59c99d77418d2
Ogaday <W.Ogaday@gmail.com>
2015-10-21 16:42:33 +0100

    I think this works. Needs more testing

--- dev/all-remote-run.sh
+++ dev/all-remote-run.sh
@@ -2,6 +2,15 @@
 
 readarray -t hosts < $1
 readarray -t jobs < $2
+if [ "$3" == "" ]; then
+  echo foo
+  casesdir=/home/ogaday/Project/CFD_Files
+else
+  echo bar
+  casedir=$3
+  casesdir=${casedir%/}
+fi
+echo casesdir is $casesdir
 
 echo "length hosts: ${#hosts[@]}"
 echo "length jobs: ${#jobs[@]}"
@@ -17,5 +26,12 @@ fi
 for ((i=0;i<$extent;++i)); do
   # echo "${hosts[i]} runs ${jobs[i]}" >> ~/Project/CFD_Files/jobtraq.txt
   echo "${hosts[i]} runs ${jobs[i]}"
+  solno=${jobs[i]#*.}
+  genid=${jobs[i]%.$solno}
+  casepath="$casesdir/generation$genid/${jobs[i]}"
+  echo "casepath: $casepath"
+  echo "gen id: $genid"
+  echo "solution no.: $solno"
+  echo "remote host: ${hosts[i]}"
+  #./remote-run.sh /home/ogaday/Project
 done
-
