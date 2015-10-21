c757d7231b2c25b90f5786aa3ab16cf171253fdb
Ogaday <W.Ogaday@gmail.com>
2015-10-21 14:22:25 +0100

    Test cases run successfully. Seems to work

--- dev/remote-run.sh
+++ dev/remote-run.sh
@@ -10,8 +10,8 @@ remotehost=${args[3]}
 echo "casepath is $casepath"
 echo "genid is $genid"
 echo "solid is $solid"
-echo remotehost "is $remotehost"
+echo "remotehost is $remotehost"
 
 ssh wow203@$remotehost "mkdir -p /scratch/wow203/OpenFOAM/wow203-2.1.0/run/generation$genid/"
-scp -r path wow203@remotehost:/scratch/wow203/OpenFOAM/wow203-2.1.0/run/generation$genid/$solid
+scp -r $casepath wow203@$remotehost:"/scratch/wow203/OpenFOAM/wow203-2.1.0/run/generation$genid/"
 ssh wow203@$remotehost "cd /scratch/wow203/OpenFOAM/wow203-2.1.0/run/generation$genid/$solid && ~/Project/Resources/dev/run.sh $genid ${args[2]}"
