180e743760cbc6cc81478faa4be6245c0b95ea53
Ogaday <W.Ogaday@gmail.com>
2015-10-22 16:38:02 +0100

    Remote hook to call postprocess.py with args over ssh

---
+++ remote-postprocess.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+host=$1
+genid=$2
+solno=$3
+
+ssh wow203@$host "python /home/links/wow203/Project/Resources/postprocess.py $genid $solno"
+scp wow203@$host:/scratch/wow203/OpenFOAM/wow203-2.1.0/run/generation$genid/$genid.$solno/$genid.$solno.csv /home/ogaday/Dropbox/Ogaday-Helena/Data\ CFD\ model/Power\ results/
