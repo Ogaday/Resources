0a70881d3474610275d50129859614f6b69313e3
Ogaday <W.Ogaday@gmail.com>
2015-10-23 16:12:52 +0100

    begin rebuild of rename

--- rename.py
+++ rename.py
@@ -2,25 +2,31 @@
 import os
 import sys
 
-if len(sys.argv) == 1:
-    old_name=sys.argv[1]
-elif len(sys.argv) == 2:
-    old_name=sys.argv[1]
-    gen = sys.argv[2]
-else:
-    old_name="s-step"
-    gen="10"
-
-    
-
-for e in [entry for entry in [entry.name for entry in os.scandir()] if entry[:len(old_name)] == old_name]:
-    
-    if len(e)-len(old_name) == 1:
-        os.rename(e, gen+'.0' + e[-1])
-    elif len(e)-len(old_name) == 2 and e[-1] == 'R':
-        os.rename(e, gen+'.0'+e[-2:]
-    elif len(e)-len(old_name) == 3 and e[-1] == 'R':
-        os.rename(e, gen+'.'e[-3:]
+'''if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        old_name=sys.argv[1]
+    elif len(sys.argv) == 2:
+        old_name=sys.argv[1]
+        gen = sys.argv[2]
     else:
-        os.rename(e, gen+'.' + e[-2:])
+        old_name="s-step"
+        gen="10"
+
         
+
+    for e in [entry for entry in [entry.name for entry in os.scandir()] if entry[:len(old_name)] == old_name]:
+        if len(e)-len(old_name) == 1:
+            os.rename(e, gen + ".0" + e[-1])
+        elif len(e)-len(old_name) == 2 and e[-1] == 'R':
+            os.rename(e, gen+'.0'+e[-2:]
+        elif len(e)-len(old_name) == 3 and e[-1] == 'R':
+            os.rename(e, gen+'.'e[-3:]
+        else:
+            os.rename(e, gen+'.' + e[-2:])
+            '''
+
+if __name__ == "__main__":
+    genid, old_name = sys.argv[1:3]
+
+    for e in [entry for entry in [entry.name for entry in os.scandir()] if entry[:len(old_name)] == old_name]:
+        print(e)
