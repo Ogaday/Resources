1e800f418c808956ecd337d889637904a5deb841
Ogaday <W.Ogaday@gmail.com>
2015-11-02 17:46:09 +0000

    Change from cp to mv because these files are massive

---
+++ rename-generations.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# script for renaming case generations from s-step3 to generation03 for instance. Does not work for generations high that 09 unfortunately.
+
+for f in $(ls); do if [ "${f:0:6}" == "s-step" ]; then mv -r $f generation0${f:6}; fi; done
