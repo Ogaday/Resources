f79717271164f24bd4d64ce3fa2bec3c49383442
Ogaday <W.Ogaday@gmail.com>
2015-11-06 11:03:38 +0000

    Refactor to be more readable, handle case of host not responding

--- find-case.sh
+++ find-case.sh
@@ -11,6 +11,22 @@ found=0    #boolean
 
 #for host in $(cat $host_list); do echo "checking host $host"; ssh wow203@$host "if [ -a /scratch/wow203/OpenFOAM/wow203-2.1.0/run/generation10/$1 ]; then echo \"found=1\"; loc=$host; echo \"case $1 is on $host\"; fi"; done
 
-for host in $(cat $host_list); do echo "checking host $host"; found=$(ssh wow203@$host "if [ -a /scratch/wow203/OpenFOAM/wow203-2.1.0/run/generation10/$1 ]; then echo 1; else echo 0; fi"); if [ $found -eq 1 ]; then echo "Found case $1 on host $host!"; loc=$host; found=0; fi; done
+for host in $(cat $host_list); do
+  echo "checking host $host"
+  if found=$(ssh wow203@$host "if [ -a /scratch/wow203/OpenFOAM/wow203-2.1.0/run/generation${1%.*}/$1 ]; then echo 1; else echo 0; fi"); then
+   :
+  else
+    found=-1
+  fi
+  if [ "$found" -eq 1 ]; then
+    echo "Found case $1 on host $host!"
+    loc=$host
+    found=0
+  elif [ "$found" -eq -1 ]; then
+    echo "Host $host unresponsive"
+  else
+    echo "Not on $host"	  
+  fi
+done
 
-if [ "$loc" != "" ]; then echo "Found! On host $loc"; else echo "Not found"; fi
+if [ "$loc" != "" ]; then echo "Found $1! On host $loc"; else echo "Not found"; fi
