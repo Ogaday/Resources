814a5d24f631d4fe7ec0e90a83b86e236cdb508e
Ogaday <W.Ogaday@gmail.com>
2015-11-06 13:12:45 +0000

    Add option to just extract raw results with a flag -v

--- postprocess.py
+++ postprocess.py
@@ -16,6 +16,16 @@ runpath = "/scratch/wow203/OpenFOAM/wow203-2.1.0/run"
 swak = "swakExpression_"
 turbines = ["R1C1", "R2C1", "R2C2", "R3C1"]
 cos = ["x", "z"]
+dirs=['swakExpression_Fx_inR1C1', 'swakExpression_Fx_outR1C1',
+      'swakExpression_Fx_inR2C1', 'swakExpression_Fx_outR2C1',
+      'swakExpression_Fx_inR2C2', 'swakExpression_Fx_outR2C2',
+      'swakExpression_Fx_inR3C1', 'swakExpression_Fx_outR3C1',
+      'swakExpression_Fz_inR1C1', 'swakExpression_Fz_outR1C1',
+      'swakExpression_Fz_inR2C1', 'swakExpression_Fz_outR2C1',
+      'swakExpression_Fz_inR2C2', 'swakExpression_Fz_outR2C2',
+      'swakExpression_Fz_inR3C1', 'swakExpression_Fz_outR3C1',
+      'swakExpression_TR1C1v', 'swakExpression_TR2C1v',
+      'swakExpression_TR2C2v', 'swakExpression_TR3C1v']
 
 def get_generation_dir(generation):
     """
@@ -64,7 +74,7 @@ def extract_power(solution_dir):
     for turbine in turbines:
         powerdict[turbine] = sum([float(get_F_in(start, co, turbine, solution_dir))-float(get_F_out(start, co, turbine, solution_dir)) for co in cos])*float(get_T(start, turbine, solution_dir))
     # create dictionary of in forces, dictionary of out forces, dictionary of velocities
-    # for each turbine, calculate power and store in powerdict 
+    # for each turbine, calculate power and store in powerdict
     return powerdict
 
 def check_for_finish(solution_dir):
@@ -76,7 +86,7 @@ def check_for_finish(solution_dir):
 def write_results(solution_dir, powerdict, filename):
     """
     Given the solution_dir and a dictionary of turbine:power, write the results to a single line csv file.
-    """ 
+    """
     results = ''
     for turbine in turbines:
         results += str(powerdict[turbine]) + ','
@@ -86,7 +96,7 @@ def write_results(solution_dir, powerdict, filename):
 def seek_last_line(file):
     """
     Take a file and return the last line in that file.
-    
+
     See https://github.com/Ogaday/PyDoodles for alternatives and benchmarks. This seems to be the fastest
     method by some orders of magnitude.
     """
@@ -116,16 +126,49 @@ def get_T(start, turbine, solution_dir):
     #last_line = seek_last_line("{}T{}v/".format(swak, turbine))
     return last_line.split()[1]
 
+def get_val(dir):
+    last_line = seek_last_line(dir)
+    return last_line.split()[1]
+
+def write_vals(solution_dir, filename):
+    vals = []
+    start=get_start_point(solution_dir)
+    for d in dirs:
+        val.append(get_val(d+"/"+start+"/"+d[15:]))
+    with open(solution_dir+filename+'raw.csv', 'w') as f:
+        f.write(','.join(vals))
+
+helpstring="""Postprocess information
+Usage:
+    $ python postprocess.py -h  -  displays this help message and exits
+    $ python postprocess.py 10 18  -  for instance calculates the power
+    extracted for case 10.18 if 10.18 is on the standard path
+    ie './scratch/wow203/OpenFOAM/wow203-2.1.0/run/generation10'.
+    $ python postprocess.py 10 18 /alternative/run/path calculates the power
+    whent he case is in another location.
+    $ python postprocess.py 10 18 -v writes the verbose results to a file.
+"""
 if __name__ == "__main__":
     # parse argument from the commandline / system.
-    gen_arg, sol_arg = sys.argv[1:3]
-    if len(sys.argv) == 4:
-        runpath = sys.argv[-1]
-        while runpath[-1] == '/' and len(runpath)>1:
-            runpath=runpath[:-1]
-    generation_dir = get_generation_dir(gen_arg)
-    print(generation_dir)
-    solution_dir = get_solution_dir(generation_dir, sol_arg)
-    print(solution_dir)
-    if check_for_finish(solution_dir):
-        write_results(solution_dir, extract_power(solution_dir), get_solution_name(generation_dir, sol_arg))
+    if '-h' in sys.argv:
+        print(helpstring)
+    else:
+        if '-v' in sys.argv:
+            verbose=True
+            sys.argv.remove('-v')
+        else:
+            verbose=False
+        gen_arg, sol_arg = sys.argv[1:3]
+        if len(sys.argv) == 4:
+            runpath = sys.argv[-1]
+            while runpath[-1] == '/' and len(runpath)>1:
+                runpath=runpath[:-1]
+        generation_dir = get_generation_dir(gen_arg)
+        print(generation_dir)
+        solution_dir = get_solution_dir(generation_dir, sol_arg)
+        print(solution_dir)
+        if check_for_finish(solution_dir):
+            if verbose:
+                write_vals(solution_dir, get_solution_name(generation_dir, sol_arg))
+            else:
+                write_results(solution_dir, extract_power(solution_dir), get_solution_name(generation_dir, sol_arg))
