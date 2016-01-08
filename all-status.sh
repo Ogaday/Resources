a464cd9d2ff771fdf489028e9f0d114d3a50ab8e
Ogaday <W.Ogaday@gmail.com>
2016-01-08 10:06:24 +0000

    Update with files and changes from workOggu

--- all-status.sh
+++ all-status.sh
@@ -7,4 +7,4 @@ readarray -t jobs < $2
 
 if [ ${#hosts[@]} -le ${#jobs[@]} ]; then extent=${#hosts[@]}; else extent=${#jobs[@]}; fi
 
-for ((i=0;i<$extent;i++)); do echo ${hosts[i]} running ${jobs[i]}; ssh wow203@${hosts[i]} "ls /scratch/wow203/OpenFOAM/wow203-2.1.0/run/generation${jobs[i]%.*}/${jobs[i]} | grep -v swak | grep -v constant | grep -v log | grep -v system"; done
+for ((i=0;i<$extent;i++)); do echo ${hosts[i]} running ${jobs[i]}; ssh wow203@${hosts[i]} "ls /scratch/wow203/OpenFOAM/wow203-2.1.0/run/generation${jobs[i]%.*}/${jobs[i]} | grep -v swak | grep -v constant | grep -v log | grep -v system" | tail -n 1; done
---
+++ status_on_6_7.sh
@@ -0,0 +1,1 @@
+./all-status.sh <(cat hosts6.txt hosts7.txt) <(cat jobs6.txt jobs7.txt)
