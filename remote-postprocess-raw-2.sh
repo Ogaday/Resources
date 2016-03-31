cd79bda9183cdb0d378c1275d8605a4a73da6cee
Ogaday <w.ogaday@gmail.com>
2016-03-31 16:12:09 +0100

    remote-postprocess-raw for running on linux machines.

---
+++ remote-postprocess-raw-2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+host=$1
+genid=$2
+solno=$3
+
+ssh wow203@$host "python /home/links/wow203/Project/Resources/postprocess.py $genid $solno -v"
+scp wow203@$host:/scratch/wow203/OpenFOAM/wow203-2.1.0/run/generation$genid/$genid.$solno/$genid.$solno-raw.csv ~/Project/CFD_Files/results 
